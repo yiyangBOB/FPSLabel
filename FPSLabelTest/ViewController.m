36c66547fa841c51a3d7d4af0f3e9de018e1f57d
yangyi <gggjjjaoia@163.com>
2016-01-12 14:33:09 +0800

    添加测试项目

---
+++ FPSLabelTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FPSLabelTest
+//
+//  Created by yangyi on 16/1/12.
+//  Copyright © 2016年 yangyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
